9b754461a23e9de2197c13b550fc9ea6fa89cab0
pchmykh <pchmykh@outlook.com>
2025-03-28 13:36:25 +0300

    cleaning tf files

--- droplet1.tf
+++ droplet1.tf
@@ -7,6 +7,7 @@ resource "digitalocean_droplet" "droplet1" {
     data.digitalocean_ssh_key.terraform1.id,
     data.digitalocean_ssh_key.terraform2.id
   ]
+
   connection {
     host = self.ipv4_address
     user = "root"
@@ -14,12 +15,21 @@ resource "digitalocean_droplet" "droplet1" {
     private_key = file(var.pvt_key)
     timeout = "2m"
   }
-  provisioner "remote-exec" {
-    inline = [
-      "export PATH=$PATH:/usr/bin",
-      # install nginx
-      "sudo apt-get update",
-      "sudo apt-get -y install nginx"
+  # Example of package installation
+  #  provisioner "remote-exec" {
+  #    inline = [
+  #      "export PATH=$PATH:/usr/bin",
+  #      # install nginx
+  #      "sudo apt-get update",
+  #      "sudo apt-get -y install nginx"
+  #    ]
+  #  }
+}
+
+# move droplet to 'test' project after creating
+resource "digitalocean_project_resources" "test_assigment" {
+  project = data.digitalocean_project.test.id
+  resources   = [
+      digitalocean_droplet.droplet1.urn
     ]
-  }
 }
