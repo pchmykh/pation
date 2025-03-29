42c43d9b8303f68ac634c946c72f7a5a87b9828c
pchmykh <125208381+pchmykh@users.noreply.github.com>
2025-03-29 18:34:26 +0300

    random DO space fix form Google

--- backend.tf
+++ backend.tf
@@ -10,8 +10,6 @@ terraform {
     
     skip_credentials_validation = true
     skip_region_validation      = true
-    skip_metadata_api_check     = true
-    skip_requesting_account_id  = true
-    force_path_style            = true
+    use_path_style            = true
   }
 }
